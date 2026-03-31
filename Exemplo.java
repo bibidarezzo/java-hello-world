6d903f06df7bbb01284c191e1312ed5a773851f8
Gabriela Darezzo Gomes <100491954+bibidarezzo@users.noreply.github.com>
2026-03-31 11:54:33 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JOptionPane;
+
+
+ 
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author 826174958
+ */
+public class Exemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        JOptionPane.showMessageDialog(null,"Hello World!");
+    }
+    
+}
